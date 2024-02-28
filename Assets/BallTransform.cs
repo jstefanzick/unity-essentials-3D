e3b97a94f9260d0507fe98f5806b4113b8850734
jstefanzick <stefanzi@seattleu.edu>
2024-02-27 19:01:56 -0800

    Ball Transform Script

---
+++ Assets/BallTransform.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTransform : MonoBehaviour
+{
+
+    public Vector3 scaleChange;
+    public Vector3 locationChange;
+    public Vector3 rotateChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += scaleChange;
+        transform.localPosition += locationChange;
+        transform.Rotate(rotateChange);
+    }
+}
